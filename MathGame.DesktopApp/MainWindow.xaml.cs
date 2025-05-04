2249891d5e520f13e06f33aae729a91fa7030e38
boar33 <boarborg@gmail.com>
2025-05-03 20:18:21 -0600

    Add project files.

---
+++ MathGame.DesktopApp/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MathGame.DesktopApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddWpfBlazorWebView();
+
+#if DEBUG
+            serviceCollection.AddBlazorWebViewDeveloperTools();
+#endif
+            Resources.Add("services", serviceCollection.BuildServiceProvider());
+        }
+    }
+}
